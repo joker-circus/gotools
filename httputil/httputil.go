dfdd48e4f8e371ba1c24d850254d956418701db9
stephen.li <lifetimeys.li@gmail.com>
2022-12-22 11:45:09 +0800

    feat: 更新 http 方法

--- httputil/httputil.go
+++ httputil/httputil.go
@@ -1,19 +1,64 @@
 package httputil
 
 import (
+	"bufio"
+	"compress/flate"
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"path"
+	"strconv"
 	"strings"
+
+	"github.com/pkg/errors"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding/htmlindex"
 )
 
 func Post(url string, body interface{}, header map[string]string) ([]byte, error) {
-	return Do(http.MethodPost, url, body, header, make(map[string]interface{}))
+	resp, err := Request(http.MethodPost, url, body, header, make(map[string]interface{}))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close() // nolint
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return respBody, nil
 }
 
 func Get(url string, query map[string]interface{}, header map[string]string) ([]byte, error) {
-	return Do(http.MethodGet, url, nil, header, query)
+	resp, err := Request(http.MethodGet, url, nil, header, query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close() // nolint
+
+	var reader io.ReadCloser
+	switch resp.Header.Get("Content-Encoding") {
+	case "gzip":
+		reader, _ = gzip.NewReader(resp.Body)
+	case "deflate":
+		reader = flate.NewReader(resp.Body)
+	default:
+		reader = resp.Body
+	}
+	defer reader.Close() // nolint
+
+	body, err := io.ReadAll(reader)
+	if err != nil && err != io.EOF {
+		return nil,err
+	}
+	return body, nil
 }
 
 // 若 body 为 string、[]byte 类型直接返回，
@@ -30,7 +75,7 @@ func BytesBody(body interface{}) ([]byte, error) {
 	return json.Marshal(body)
 }
 
-func Do(method, url string, body interface{}, header map[string]string, query map[string]interface{}) ([]byte, error) {
+func Request(method, url string, body interface{}, header map[string]string, query map[string]interface{}) (*http.Response, error) {
 	client := &http.Client{}
 
 	byteParams, err := BytesBody(body)
@@ -41,7 +86,7 @@ func Do(method, url string, body interface{}, header map[string]string, query ma
 	var req *http.Request
 	req, err = http.NewRequest(method, url, strings.NewReader(string(byteParams)))
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(err)
 	}
 
 	for k, v := range header {
@@ -58,15 +103,107 @@ func Do(method, url string, body interface{}, header map[string]string, query ma
 	var resp *http.Response
 	resp, err = client.Do(req)
 	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("resp.StatusCode is %v, req body is %+v", resp.StatusCode, string(byteParams))
+	}
+
+	return resp, err
+}
+
+// Size get size of the header
+func Size(h http.Header) (int64, error) {
+	s := h.Get("Content-Length")
+	if s == "" {
+		return 0, errors.New("Content-Length is not present")
+	}
+	size, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return size, nil
+}
+
+// ContentType get Content-Type of the header
+func ContentType(h http.Header) (string, error) {
+	s := h.Get("Content-Type")
+	// handle Content-Type like this: "text/html; charset=utf-8"
+	return strings.Split(s, ";")[0], nil
+}
+
+// M3u8URLs get all urls from m3u8 url
+func M3u8URLs(uri string) ([]string, error) {
+	if len(uri) == 0 {
+		return nil, errors.New("url is null")
+	}
+
+	html, err := Get(uri, nil, nil)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	lines := strings.Split(string(html), "\n")
+	var urls []string
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line != "" && !strings.HasPrefix(line, "#") {
+			if strings.HasPrefix(line, "http") {
+				urls = append(urls, line)
+			} else {
+				base, err := url.Parse(uri)
+				if err != nil {
+					continue
+				}
+				u, err := url.Parse(line)
+				if err != nil {
+					continue
+				}
+				urls = append(urls, base.ResolveReference(u).String())
+			}
+		}
+	}
+	return urls, nil
+}
+
+// 获取 body 的编码格式
+func detectContentCharset(body io.Reader) string {
+	r := bufio.NewReader(body)
+	if data, err := r.Peek(1024); err == nil {
+		if _, name, ok := charset.DetermineEncoding(data, ""); ok {
+			return name
+		}
+	}
+	return "utf-8"
+}
+
+// 返回 UTF-8 的 html 文本。
+// DecodeHTMLBody returns an decoding reader of the html Body for the specified `charset`
+// If `charset` is empty, DecodeHTMLBody tries to guess the encoding from the content
+func DecodeHTMLBody(body io.Reader, charsets ...string) (io.Reader, error) {
+	var charsetName string
+	if len(charsets) == 0 {
+		charsetName = detectContentCharset(body)
+	} else {
+		charsetName = charsets[0]
+	}
+
+	e, err := htmlindex.Get(charsetName)
+	if err != nil {
 		return nil, err
 	}
-
-	defer resp.Body.Close()
-
-	repBody, err := ioutil.ReadAll(resp.Body)
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("resp.StatusCode is %v, resp body is %+v, req body is %+v", resp.StatusCode, string(repBody), string(byteParams))
+	if name, _ := htmlindex.Name(e); name != "utf-8" {
+		body = e.NewDecoder().Reader(body)
 	}
+	return body, nil
+}
 
-	return repBody, err
+func JoinPaths(uri string, paths ... string) string {
+	urlObj, _ := url.Parse(uri)
+	newPath := make([]string, 0, len(paths) + 1)
+	newPath = append(newPath, urlObj.Path)
+	newPath = append(newPath, paths...)
+	urlObj.Path = path.Join(newPath...)
+
+	return urlObj.String()
 }
