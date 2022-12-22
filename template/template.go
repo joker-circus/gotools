319504afeb10fb72c848fd3ad1f57089cd87fdff
stephen.li <lifetimeys.li@gmail.com>
2022-12-22 14:25:07 +0800

    feat: template 渲染方法增加可选项

--- template/template.go
+++ template/template.go
@@ -3,28 +3,70 @@ package template
 import (
 	"bytes"
 	"fmt"
+	"regexp"
 	"strings"
 	"text/template"
 )
 
+// RenderFuncMap 是 Render 系列方法默认的 Template.FuncMap
+var RenderFuncMap template.FuncMap
+
+// StringsFuncMap 提供 strings 的 Contains、Replace、HasSuffix、HasPrefix、Title、
+// ToTitle、ToLower、ToUpper、Split、Trim、TrimSpace、Join方法
+var StringsFuncMap = template.FuncMap{
+	"Contains":  strings.Contains,
+	"Replace":   strings.ReplaceAll,
+	"HasSuffix": strings.HasSuffix,
+	"HasPrefix": strings.HasPrefix,
+	"Title":     strings.Title,
+	"ToTitle":   strings.ToTitle,
+	"ToLower":   strings.ToLower,
+	"ToUpper":   strings.ToUpper,
+	"Split":     strings.Split,
+	"Trim":      strings.Trim,
+	"TrimSpace": strings.TrimSpace,
+	"Join":      strings.Join,
+}
+
+func init() {
+	RenderFuncMap = StringsFuncMap
+}
+
+// Render 默认替换文本中 {{ if }} 等语句带来的多行
 func Render(templateContent string, varsMap interface{}) (string, error) {
+	return RenderWithOptions(templateContent, varsMap, ReplaceMultipleLine)
+}
+
+func RenderWithOptions(content string, varsMap interface{}, options ...RenderOption) (string, error) {
+	result, err := render(content, varsMap, RenderFuncMap)
+	if err != nil {
+		return "", err
+	}
+
+	for _, option := range options {
+		result = option(result)
+	}
+
+	return result, nil
+}
+
+func render(content string, varsMap interface{}, funcMap template.FuncMap) (string, error) {
 	// 渲染
-	tmpl, err := template.New("default").Option("missingkey=error").Parse(templateContent)
+	tmpl, err := template.New("default").Funcs(funcMap).Option("missingkey=error").Parse(content)
 	if err != nil {
 		return "", err
 	}
 	buf := new(bytes.Buffer)
 	err = tmpl.Execute(buf, varsMap)
 	if err != nil {
-		return "", RenderErrFormat(err)
+		return "", renderErrFormat(err)
 	}
 
-	// 替换文本中 {{ if }} 等语句带来的多行
-	return strings.ReplaceAll(buf.String(), "\n\n", "\n"), nil
+	return buf.String(), nil
 }
 
 // simplify error
-func RenderErrFormat(err error) error {
+func renderErrFormat(err error) error {
 	if strings.Contains(err.Error(), "map has no entry") {
 		tmpSlice := strings.Split(err.Error(), " ")
 		missingKey := tmpSlice[len(tmpSlice)-1]
@@ -32,4 +74,20 @@ func RenderErrFormat(err error) error {
 		return fmt.Errorf("%s was not defined", missingKey)
 	}
 	return err
+}
+
+// RenderOption 用于对渲染后的文本信息进行额外处理
+type RenderOption func(string) string
+
+// 等同于 “\\n+” 正则，二次转义
+var multipleLineReg = regexp.MustCompile(`\n+`)
+
+// 替换多余的空行
+func ReplaceMultipleLine(s string) string {
+	return multipleLineReg.ReplaceAllString(s, "\n")
+}
+
+// 替换 & -> &amp;
+func ReplaceAmp(s string) string {
+	return strings.ReplaceAll(s, "&", "&amp;")
 }
--- template/template_test.go
+++ template/template_test.go
@@ -2,16 +2,19 @@ package template
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type _Test struct {
-	Status         string    `json:"status" gorm:"column:status"`
-	Business       string    `json:"business" gorm:"column:business"`
-	Service        string    `json:"service" gorm:"column:service"`
-	Cluster        string    `json:"cluster" gorm:"column:cluster"`
-	UpdatedAt       time.Time `json:"updated_at" gorm:"column:deadline"`
+	Status    string    `json:"status" gorm:"column:status"`
+	Business  string    `json:"business" gorm:"column:business"`
+	Service   string    `json:"service" gorm:"column:service"`
+	Cluster   string    `json:"cluster" gorm:"column:cluster"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"column:deadline"`
 }
 
 func (c _Test) StatusCN(status string) string {
@@ -19,18 +22,17 @@ func (c _Test) StatusCN(status string) string {
 }
 
 var _date = _Test{
-	Status:         "new",
-	Business:       "测试业务",
-	Service:        "test-service",
-	Cluster:        "测试集群",
-	UpdatedAt:       time.Now(),
-
+	Status:    "new",
+	Business:  "测试业务",
+	Service:   "test-service",
+	Cluster:   "测试集群",
+	UpdatedAt: time.Now(),
 }
 
 func TestRender(t *testing.T) {
 	templateContent := `
 Business：{{.Business}}
-Service：{{.Service}}
+Service：{{Title .Service}}
 {{ if .Cluster }}
 Cluster：{{.Cluster}}
 {{ end }}
@@ -42,5 +44,13 @@ UpdatedAt：{{.UpdatedAt.Format "2006-01-02 15:04:05"}}`
 		t.Error(err.Error())
 		return
 	}
-	fmt.Println(content)
+
+	result := fmt.Sprintf(`
+Business：%s
+Service：%s
+Cluster：%s
+StatusCN：%s
+UpdatedAt：%s`, _date.Business, strings.Title(_date.Service), _date.Cluster,
+		_date.StatusCN(_date.Status), _date.UpdatedAt.Format("2006-01-02 15:04:05"))
+	assert.Equal(t, result, content, "the should be equal")
 }
